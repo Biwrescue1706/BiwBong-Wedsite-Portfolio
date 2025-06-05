e74ce7755e568d31af1a208642272421f33554d7
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-06-05 16:19:09 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navList = document.getElementById("nav-list");
-
-menuToggle.addEventListener("click", () => {
-  navList.classList.toggle("show");
-});
